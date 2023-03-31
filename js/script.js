42b008c436898dccb175c2b5e1f7f9c520e9b713
Nikita <sinkvadev@gmail.com>
2023-03-31 08:50:30 +0700

    deadline timer fix

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ import cards from './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
 import slider from './modules/slider';
-import {openModal} from './modules/modal';
+import { openModal } from './modules/modal';
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2022-06-11');
+    timer('.timer', '2023-06-11');
     cards();
     calc();
     forms('form', modalTimerId);
